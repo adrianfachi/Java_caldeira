62816b4221903bcce65855b345db96a88bd67bdc
adrianfachi <133840517+adrianfachi@users.noreply.github.com>
2023-10-16 18:22:17 -0300

    Exercicios aula03

---
+++ Aula03/src/Exercicio02.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Exercicio02 {
+    public static void main(String[] args) {
+        List<Integer> numeros = new ArrayList<>();
+        Random criaNumeros = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < 10; i++) {
+            numeros.add(criaNumeros.nextInt(1,101));
+        }
+
+        System.out.println("Digite um número: ");
+        if (numeros.contains(scanner.nextInt())){
+            System.out.println("Você acertou, o número está entre os números");
+        } else {
+            System.out.println("Você errou, o número não está entre os números");
+        }
+    }
+}
