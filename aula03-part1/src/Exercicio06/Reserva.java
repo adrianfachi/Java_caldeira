454aad2ade087f033fce7e3e942ab341bf3c9435
adrianfachi <133840517+adrianfachi@users.noreply.github.com>
2023-10-17 16:53:00 -0300

    Exercicios aula03

---
+++ aula03-part1/src/Exercicio06/Reserva.java
@@ -0,0 +1,21 @@
+package Exercicio06;
+
+public class Reserva {
+    private String nome;
+    private String local;
+    private String dataEntrada;
+    private String dataSaida;
+
+    public Reserva (String nome, String local, String dataEntrada, String dataSaida) {
+        this.nome = nome;
+        this.local = local;
+        this.dataEntrada = dataEntrada;
+        this.dataSaida = dataSaida;
+    }
+
+    public String exibirReserva() {
+        return nome + " reservou em " + local + " do dia " + dataEntrada + " até " + dataSaida;
+    }
+
+    
+}
